02e2ad4ec20ec6b50410c09ba04ac9a40cf1a12c
Yulia Yachnik <Yachnik_y@mail.ru>
2017-06-20 22:06:46 +0300

    готовый билдер

--- src/main/java/org/ApplicationEntryPoint.java
+++ src/main/java/org/ApplicationEntryPoint.java
@@ -2,6 +2,10 @@ package org;
 
 import org.services.CommandDefinition;
 import org.services.ParametrDefinitions;
+import org.services.add.AddCommandImpl;
+import org.services.find.FindCommandImpl;
+import org.services.help.HelpCommandImpl;
+import org.services.list.ListCommandImpl;
 
 import java.util.*;
 
@@ -11,23 +15,48 @@ import java.util.*;
 public class ApplicationEntryPoint {
     public static void main(String args[]) throws ClassNotFoundException {
 
-        List <ParametrDefinitions> parametrDefinitionsList = new ArrayList <ParametrDefinitions> ();
-        ParametrDefinitions parametrDefinitions = new ParametrDefinitions("name",true);
-        ParametrDefinitions parametrDefinitions1 = new ParametrDefinitions("phone",true);
-        parametrDefinitionsList.add(parametrDefinitions);
-        parametrDefinitionsList.add(parametrDefinitions1);
-        Class command = Class.forName("org.services.add.AddCommandImpl");
-        Class commandFind = Class.forName("org.services.find.FindCommandImpl");
+        List <ParametrDefinitions> addParametrDefinitionsList = new ArrayList <ParametrDefinitions> ();
+        List <ParametrDefinitions> findParametrDefinitionsList = new ArrayList<>();
+        List <ParametrDefinitions> listParametrDefinitionsList = new ArrayList<>();
+        List <ParametrDefinitions> helpParametrDefinitionsList = new ArrayList<>();
+
+        ParametrDefinitions nameParametrDefinitions = new ParametrDefinitions("name",true);
+        ParametrDefinitions phoneParametrDefinitions = new ParametrDefinitions("phone",true);
+        ParametrDefinitions fileParametrDefinitions = new ParametrDefinitions("filename", false);
+        ParametrDefinitions dirParametrDefinitions = new ParametrDefinitions("dirname",true);
+
+        addParametrDefinitionsList.add(nameParametrDefinitions);
+        addParametrDefinitionsList.add(phoneParametrDefinitions);
+        addParametrDefinitionsList.add(fileParametrDefinitions);
+        addParametrDefinitionsList.add(dirParametrDefinitions);
+
+        findParametrDefinitionsList.add(nameParametrDefinitions);
+        findParametrDefinitionsList.add(fileParametrDefinitions);
+        findParametrDefinitionsList.add(dirParametrDefinitions);
+
+        listParametrDefinitionsList.add(fileParametrDefinitions);
+        listParametrDefinitionsList.add(dirParametrDefinitions);
+
         Map<String,String>  map = new HashMap<String, String>();
-        map.put("--filename","myfile.txt");
-        map.put("--dir","myfile");
+        map.put("--filename","phonebook.txt");
+        map.put("--dirname","phonebook");
+
+        Class commandAdd  =  AddCommandImpl.class;
+        Class commandFind = FindCommandImpl.class;
+        Class commandList = ListCommandImpl.class;
+        Class commandHelp = HelpCommandImpl.class;
+
 
 
          PhoneBookApplication phoneBookApplication = new PhoneBookApplicationBuilder()
-                 .withCommands(new CommandDefinition("add", parametrDefinitionsList,command))
+                 .withCommands(new CommandDefinition("add", addParametrDefinitionsList,commandAdd))
                  .withConfig(map)
-                 .withCommands(new CommandDefinition("find", parametrDefinitionsList,commandFind))
-                .withConfig(map)
+                 .withCommands(new CommandDefinition("find", findParametrDefinitionsList, commandFind))
+                 .withConfig(map)
+                 .withCommands(new CommandDefinition("list", listParametrDefinitionsList, commandList))
+                 .withConfig(map)
+                 .withCommands(new CommandDefinition("help", helpParametrDefinitionsList, commandHelp))
+                 .withConfig(map)
                 .build();
 
         phoneBookApplication.run(args);
--- src/main/java/org/PhoneBookApplication.java
+++ src/main/java/org/PhoneBookApplication.java
@@ -11,17 +11,29 @@ import java.util.Map;
 public class PhoneBookApplication {
     private List <CommandDefinition> commandDefinition;
     private Map<String,String> params;
+    private Map <String,CommandDefinition> commandDefinitionMap;
 
     public PhoneBookApplication(List <CommandDefinition> commandDefinition, Map<String, String> params) {
         this.commandDefinition = commandDefinition;
         this.params = params;
+        commandDefinitionMap = new HashMap<>();
+        for(int i = 0; i < commandDefinition.size(); i++) {
+            commandDefinitionMap.put(commandDefinition.get(i).getName(), commandDefinition.get(i));
+        }
     }
 
     public void run(String args[]){
+        String commandName = args[0];
+        try{
+            Command  command = commandDefinitionMap.get(commandName).getCommand().newInstance();
 
-       // System.out.println(this.commandDefinition);
-       // System.out.println(this.params);
-        String name = args[0];
+        }catch (NullPointerException e){
+            System.out.println("Such method no definite");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
 
     }
 }
